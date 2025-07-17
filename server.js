1cbde722a5246ad68a92e79aede06525734527ca
Anumol-dot <99953075+Anumol-dot@users.noreply.github.com>
2025-07-17 18:19:43 +0530

    Fix host binding for Render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cardRoutes = require('./routes/card');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const PORT = process.env.PORT || 3000;
  app.use(bodyParser.json());
  app.use(bodyParser.urlencoded({extended: true}));
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
@@ -21,9 +21,12 @@ app.use('/api/cards', cardRoutes);
 // Connect DB and start server
 connectDB().then(() => {
  // importDB();
-  app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+ app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+  // app.listen(3000, () => {
+  //   console.log('Server is running on http://localhost:3000');
+  // });
 });
 
 // app.listen(PORT, () => {
